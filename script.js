219579f517ced13eba571af194dd5a674026f92e
Atul Mishra <meatulmishra22@gmail.com>
2024-02-17 21:03:57 +0530

    my commit

--- script.js
+++ script.js
@@ -8,20 +8,40 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(student => {if(student.marks > 50){
+    console.log(student);
+  }})
+
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(student => {if(student.marks > 50){
+    console.log(student);
+  }})
 }
 
 function addData() {
   //Write your code here, just console.log
+   const newObj = {id:4,name:"susan",age:"20",marks:45};
+   arr.push(newObj);
+   console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  const UpdatedArr = arr.filter(student => student.marks >= 50)
+  console.log(UpdatedArr);
+
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const arr2 = [
+    { id: 4, name: "john Abraham", age: "28", marks: 60 },
+    { id: 5, name: "jacki Dada", age: "30", marks: 75 },
+    { id: 6, name: "karun Nair", age: "29", marks: 45 },
+  ];
+  const newArr = arr.concat(arr2);
+  console.log(newArr);
 }
